cfd2739ed399d2ea3d2420bd8ec39237b62ce9e6
Roger Chao <rogman@gmail.com>
2020-09-23 10:30:45 -0700

    first commit

---
+++ main.tf
@@ -0,0 +1,35 @@
+variable "gcp_credential" {
+  default = "foo"
+}
+
+resource "local_file" "gcp_credential" {
+    sensitive_content  = var.gcp_credential
+    filename = "${path.module}/gcp_credential.json"
+}
+
+provider "google" {
+  credentials = file("${path.module}/gcp_credential.json")
+  project     = var.gcp_project
+  region      = var.gcp_region
+}
+
+resource "google_compute_instance" "demo" {
+  count        = var.instance_count
+  name         = var.instance_name
+  machine_type = var.machine_type
+  zone         = var.gcp_zone
+
+  boot_disk {
+    initialize_params {
+      image = var.image
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral IP
+    }
+  }
+}
